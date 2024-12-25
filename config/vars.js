2500f51b8d32cb539d87042cdc6464840bc94af5
Neon4sure! <168220845+Manaseditz10@users.noreply.github.com>
2024-12-25 17:50:47 +0530

    Add files via upload

---
+++ config/vars.js
@@ -0,0 +1,21 @@
+const vars = {
+    token: "MTIzMjk1MzExNTU0OTExMDMxMw.GRzUq4.tfwPV_TIQrp9LHFQeYqQn6gYvGfy56iBj5uZXY",
+    prefix: "!",
+    mongoURL: "mongodb://localhost:27017/lyra_test",
+    owners: ["1231645547668967488"],
+    color: "#ff0000" ,// embed color
+    webhooks: {
+        cmd: ""
+    },
+    nodes: [{
+        name: "Lyra",
+        url: "localhost:2333",
+        auth: "avon",
+        secure: false
+    }],
+    serverLink: "https://discord.gg/lyra",
+    supportId: "",
+    setupImg: null
+}
+
+module.exports = vars;
